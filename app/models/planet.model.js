91c77110d7d09e1376cb6303af34e57a2e25f3fa
kleitonalbuquerque <kleiton2102@gmail.com>
2019-04-24 23:09:18 -0300

    Correção do nome da collection(tabela) de Planet para Planets - exportação da database

--- app/models/planet.model.js
+++ app/models/planet.model.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const PlanetSchema = mongoose.Schema({
     nome: String,
     clima: String,
-    terreno: String
+    terreno: String,
+    filmes: String
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('Planet', PlanetSchema);
+module.exports = mongoose.model('Planets', PlanetSchema);
